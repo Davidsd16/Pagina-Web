fd773177b2964ce7662ab645d1c077eb645687d8
Davidsd16 <davidsanchezdiaz72@gmail.com>
2021-10-12 18:29:53 +0200

    Everything finished except image-ligth in gallery

--- html/js/menu.js
+++ html/js/menu.js
@@ -1,8 +1,8 @@
 const hamburgesa = document.querySelector('.hamburgesa')
 const menu = document.querySelector('.menu-navegacion')
 
-console.log(menu)
-console.log(hamburgesa)
+/* console.log(menu)
+console.log(hamburgesa) */
 
 hamburgesa.addEventListener('click', ()=>{
     menu.classList.toggle("spread")
